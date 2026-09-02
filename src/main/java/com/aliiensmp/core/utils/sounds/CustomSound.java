e260a27f9743ee378d2a732dcda5b9ce6056d8d8
andsilva1504 <andrevazesilva@gmail.com>
2026-09-02 21:16:25 +0100

    CustomSound#play can now receive a CommandSender instead of a Player (you can still use Player), it will internally check the instance of CommandSender before attempting to play a sound

--- src/main/java/com/aliiensmp/core/utils/sounds/CustomSound.java
+++ src/main/java/com/aliiensmp/core/utils/sounds/CustomSound.java
@@ -1,6 +1,7 @@
 package com.aliiensmp.core.utils.sounds;
 
 import org.bukkit.Sound;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,32 +12,50 @@ import org.jetbrains.annotations.Nullable;
 public record CustomSound(String soundKey, @Nullable Sound vanillaSound, float volume, float pitch) {
 
     /**
-     * Plays this specific sound to the designated player.
-     * Automatically routes to vanilla or custom sound based on availability.
+     * Plays this specific sound directly to a {@link Player}.
      *
-     * @param player The player who will hear the sound.
-     * @param isEnabled A setting value to decide whether to actually play the sound or not
+     * @param player The {@link Player} who will hear the sound.
      */
-    public void play(Player player, boolean isEnabled) {
-        if (isEnabled)
-            play(player);
+    public void play(Player player) {
+        if (vanillaSound != null) {
+            player.playSound(player.getLocation(), vanillaSound, volume, pitch);
+        } else if (soundKey != null && !soundKey.isEmpty()) {
+            player.playSound(player.getLocation(), soundKey, volume, pitch);
+        }
     }
 
     /**
-     * Plays this specific sound to the designated player.
-     * Automatically routes to vanilla or custom sound based on availability.
+     * Safely attempts to play this sound to a {@link CommandSender}.
      *
-     * @param player The player who will hear the sound.
+     * @param potentialPlayer The {@link CommandSender} who might be a player.
      */
-    public void play(Player player) {
-        if (player == null) return;
+    public void play(CommandSender potentialPlayer) {
+        if (potentialPlayer instanceof Player player) {
+            play(player);
+        }
+    }
 
-        if (vanillaSound != null) {
-            // It's a vanilla Bukkit sound
-            player.playSound(player.getLocation(), vanillaSound, volume, pitch);
-        } else if (soundKey != null && !soundKey.isEmpty()) {
-            // It's a custom resource pack sound
-            player.playSound(player.getLocation(), soundKey, volume, pitch);
+    /**
+     * Conditionally plays this sound directly to a {@link Player}.
+     *
+     * @param player The {@link Player} who will hear the sound.
+     * @param isEnabled A setting value to decide whether to actually play the sound.
+     */
+    public void play(Player player, boolean isEnabled) {
+        if (isEnabled) {
+            play(player);
+        }
+    }
+
+    /**
+     * Safely and conditionally attempts to play this sound to a {@link CommandSender}.
+     *
+     * @param potentialPlayer The {@link CommandSender} who might be a player.
+     * @param isEnabled A setting value to decide whether to actually play the sound.
+     */
+    public void play(CommandSender potentialPlayer, boolean isEnabled) {
+        if (isEnabled) {
+            play(potentialPlayer);
         }
     }
 }
